cdd55a2f0b9e81ef36c2a0b4244d2f0a485d0904
Sina Karimi <sina.k.career@gmail.com>
2025-10-05 18:15:44 +0330

    [feature]Added support for integers and keywords with identifiers
    read until page 20 of the book. in this section we implemented the lookup for keywords and identifiers along with support for integers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,9 +31,39 @@ func (l *Lexer) readChar() {
 	l.nextPosition++ // incrementing the lookahead
 }
 
+// readIdentifier reads the input until it reaches a non letter chars and then slices the input
+func (l *Lexer) readIdentifier() string {
+	position := l.currentPosition
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.currentPosition]
+}
+
+// readNumber reads the input until it reaches a non digit chars and then slices the input
+func (l *Lexer) readNumber() string {
+	position := l.currentPosition
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.currentPosition]
+}
+
+// skipWhiteSpace skips white spaces characters so we can parse actual characters
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
 // NextToken look at New to find out why we have read the character as without reading the first character
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhiteSpace()
+
 	switch l.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, l.ch)
@@ -54,10 +84,34 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			identifier := l.readIdentifier()
+			tok.Literal = identifier
+			tok.Type = token.LookupIdent(identifier)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
 	}
 
 	l.readChar()
 	return tok
+}
+
+// isLetter checks whether a character is allowed in an identifier or not
+func isLetter(character byte) bool {
+	return 'a' <= character && character <= 'z' ||
+		'A' <= character && character <= 'Z' ||
+		character == '_'
+}
+
+func isDigit(character byte) bool {
+	return '0' <= character && character <= '9'
 }
 
 func newToken(tt token.TokenType, ch byte) token.Token {
